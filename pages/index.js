77fad81e4d83c5428fa81daa758c4618078dcd43
Massimo Di Berardino <massi@Massimos-MacBook-Air.local>
2021-05-21 22:30:03 -0300

    Dynamic routes lesson :party:

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
@@ -14,6 +16,7 @@ export async function getStaticProps() {
 
 export default function Home({ allPostsData }) {
   return (
+
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
@@ -30,15 +33,18 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
       </section>
     </Layout>
+    
   )
 }
